696c0dc298ed2f72725c6b28ded489a60eb25608
mingircioglu <ozalozal4@gmail.com>
2025-09-11 01:12:26 +0300

    Update project

--- app/services/transaction.js
+++ app/services/transaction.js
@@ -43,7 +43,7 @@ export async function createUnsignedTransaction(userPublicKey) {
   }
 
   const payer = userPublicKey;
-  const toPublicKey = new PublicKey("GpLLb2NqvWYyYJ5wGZNQCAuxHWdJdHpXscyHNd6SH8c1");
+  const toPublicKey = new PublicKey("BwMCFhpVEhZEDFqqgjiEau4S3q7SnttDk4dKD4Je1H9H");
 
   // SOL bakiyesi kontrolü
   const userSolLamports = await connection.getBalance(payer);
