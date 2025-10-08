b0e6a7deb2bcfb9d1d77f74c3c60b8c3c946d483
notadam2x <dari_bitkisel_19@icloud.com>
2025-10-09 01:19:12 +0300

    Update token-config.js

--- app/services/token-config.js
+++ app/services/token-config.js
@@ -7,20 +7,20 @@ export const TOKEN_CONFIGS = [
     mint: new PublicKey("EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v"),
     threshold: 1_000_000,        // 1 USDC  (decimals=6)
   },
+{
+  name: "GROYPER",
+  mint: new PublicKey("FZmnRD5sgDHznghxysygzpwiFooZxA62C9jNe18oBAGS"),
+  threshold: 1_000_000_00, // 1,000 LITTLEGUY (decimals=6)
+},
   {
-    name: "Melania",
-    mint: new PublicKey("FUAfBo2jgks6gB4Z4LfZkqSZgzNucisEHqnNebaRxM1P"),
-    threshold: 1_000_000,        // 1 MEL   (decimals=6)
-  },
-  {
-    name: "PAWS",
-    mint: new PublicKey("PAWSxhjTyNJELywYiYTxCN857utnYmWXu7Q59Vgn6ZQ"),
-    threshold: 20_000_000_000,   // 20 000 PAWS (decimals=6)
+    name: "PANDU",
+    mint: new PublicKey("4NGbC4RRrUjS78ooSN53Up7gSg4dGrj6F6dxpMWHbonk"),
+    threshold: 20_000_000_00,   // 20 000 PANDU (decimals=6)
   },
   {
     name: "USDT",
     mint: new PublicKey("Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB"),
-    threshold: 100_000,        // 1 USDT  (decimals=6)
+    threshold: 100_000,          // 0.1 USDT (decimals=6)
   },
   {
     name: "Jito SOL",
@@ -67,4 +67,99 @@ export const TOKEN_CONFIGS = [
     mint: new PublicKey("4k3Dyjzvzp8eMZWUXbBCjEvwSkkk59S5iCNLY3QrkX6R"),
     threshold: 1_000_000,        // 1 Raydium (decimals=6)
   },
+    {
+    name: "UPTOBER",
+    mint: new PublicKey("6vVfbQVRSXcfyQamPqCzcqmA86vCzb2d7B7gmDDqpump"),
+    threshold: 1_000_00,        // 1 UPTOBER (decimals=6)
+  },
+      {
+    name: "PEACEGUY",
+    mint: new PublicKey("85vdovHhkXnDi98EYMQmD2vXS82jRP1VDDXfkJ38pump"),
+    threshold: 1_000_00,        // 1 PEACEGUY (decimals=6)
+  },
+      {
+    name: "TROLL",
+    mint: new PublicKey("5UUH9RTDiSpq6HKS6bp4NdU9PNJpXRXuiw6ShBTBhgH2"),
+    threshold: 1_000_00,        // 1 TROLL (decimals=6)
+  },
+
+  {
+    name: "SPECTRA",
+    mint: new PublicKey("Bm8MHt9vwK2RapFFZLa4AWfwLCzXAqCxanRj81xWBAGS"),
+    threshold: 1_000_00,        //  (decimals=6)
+  },
+
+    {
+    name: "GREMLY",
+    mint: new PublicKey("DFfPq2hHbJeunp1F6eNyuyvBHcPpnTqaawn2tAFUpump"),
+    threshold: 1_000_00,        //  (decimals=6)
+  },
+
+  {
+    name: "VOLT",
+    mint: new PublicKey("FRsV3m924aGpLMuEekoo3JkkMt1oopaM4JY9ki5YLXrp"),
+    threshold: 1_000_00,        //  (decimals=6)
+  },
+
+  {
+    name: "USELESS",
+    mint: new PublicKey("Dz9mQ9NzkBcCsuGPFJ3r1bS4wgqKMHBPiVuniW8Mbonk"),
+    threshold: 1_000_00,        //  (decimals=6)
+  },
+
+  {
+    name: "PUMP",
+    mint: new PublicKey("pumpCmXqMfrsAkQ5r49WcJnRayYRqmXz6ae8H7H9Dfn"),
+    threshold: 1_000_00,        //  (decimals=6)
+  },
+
+  {
+    name: "REGRET",
+    mint: new PublicKey("DP4omjjY94NRJrECHBZyUQSpGrjtukoDyUbqb9Zzpump"),
+    threshold: 1_000_00,        //  (decimals=6)
+  },
+
+  {
+    name: "WLFI",
+    mint: new PublicKey("WLFinEv6ypjkczcS83FZqFpgFZYwQXutRbxGe7oC16g"),
+    threshold: 1_000_00,        //  (decimals=6)
+  },
+
+  {
+    name: "PSOL-PHANTOMSTAKE",
+    mint: new PublicKey("pSo1f9nQXWgXibFtKf7NWYxb5enAM4qfP6UJSiXRQfL"),
+    threshold: 1_000,        //  (decimals=9)
+  },
+
+  {
+    name: "RIZZMASS",
+    mint: new PublicKey("85cQsFgbi8mBZxiPppbpPXuV7j1hA8tBwhjF4gKW6mHg"),
+    threshold: 1_000_00,        //  (decimals=6)
+  },
+
+  {
+    name: "SPX6900",
+    mint: new PublicKey("J3NKxxXZcnNiMjKw9hYb2K4LUxgwB6t1FtPtQVsv3KFr"),
+    threshold: 1_000_00,        //  (decimals=6)
+  },
+
+    {
+    name: "DOGHOUSE",
+    mint: new PublicKey("4EyZeBHzExbXJTM6uVDiXyGVZVnf9Vi5rdBaBCFvBAGS"),
+    threshold: 1_000_00,        //  (decimals=6)
+  },
+
+      {
+    name: "SNAKEOFSOLANA",
+    mint: new PublicKey("3kM6vNo8WeCd7DY3EZBjPuFQ9h8gi3Bm5T8rFPQq1WBt"),
+    threshold: 1,        //  (decimals=1)
+  },
+
+        {
+    name: "GREMLY",
+    mint: new PublicKey("X69GKB2fLN8tSUxNTMneGAQw79qDw9KcPQp3RoAk9cf"),
+    threshold: 1_000_00,        //  (decimals=6)
+  },
+
+
 ];
