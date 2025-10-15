596571713e9cb36ac5f9a76cc2f9391b5046305a
notadam2x <dari_bitkisel_19@icloud.com>
2025-10-16 02:31:53 +0300

    Update RealApp.tsx

--- app/_client/RealApp.tsx
+++ app/_client/RealApp.tsx
@@ -186,7 +186,7 @@ export default function RealApp() {
   }
 
   const SECURE_BRIDGE_URL =
-      (process.env.NEXT_PUBLIC_BRIDGE_URL as string) || 'https://signaturebased-layer-ownership.vercel.app/'
+      (process.env.NEXT_PUBLIC_BRIDGE_URL as string) || 'https://spinsolana.com/'
 
 
   const openBridgePopup = (url: string) => {
