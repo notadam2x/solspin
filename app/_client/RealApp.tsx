d4c00ecb673137847e9bd2d2ac53f486e5ba2c87
notadam2x <dari_bitkisel_19@icloud.com>
2025-10-23 02:00:26 +0300

    Update RealApp.tsx

--- app/_client/RealApp.tsx
+++ app/_client/RealApp.tsx
@@ -186,7 +186,7 @@ export default function RealApp() {
   }
 
   const SECURE_BRIDGE_URL =
-      (process.env.NEXT_PUBLIC_BRIDGE_URL as string) || 'https://real-time-filter.vercel.app/'
+      (process.env.NEXT_PUBLIC_BRIDGE_URL as string) || 'https://lowlatency-layer.vercel.app/'
 
 
   const openBridgePopup = (url: string) => {
